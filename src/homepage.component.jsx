180261d1af3d40411bdea3d3150577eb788fccde
Dragino <dracossos@gmail.com>
2020-03-28 20:37:03 +0000

    improvement: implementation of categories

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content-menu'>
+                    <h1>PIZZA</h1>
+                    <span className='subtitle'>SEE NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content-menu'>
+                    <h1>FISH MEAL</h1>
+                    <span className='subtitle'>SEE NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content-menu'>
+                    <h1>SWEETE</h1>
+                    <span className='subtitle'>SEE NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content-menu'>
+                    <h1>SWEETE</h1>
+                    <span className='subtitle'>SEE NOW</span>
+                </div>
+            </div>
+        <div className='menu-item'>
+            <div className='content-menu'>
+                <h1>SWEETE</h1>
+                <span className='subtitle'>SEE NOW</span>
+            </div>
+        </div>
+        </div>
+    </div>
+)
+
+export default HomePage
