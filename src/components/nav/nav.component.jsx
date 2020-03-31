d7c5941bd740fdf98a7c36270011879ca1f44458
Erik Dobeš <dobes@simplo.cz>
2020-03-31 16:57:55 +0100

    feat : nav template from react bootstrap

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,63 +1,39 @@
 import React from 'react';
+import { Nav, NavDropdown } from 'react-bootstrap';
 
-    class Nav extends React.Component {
+class MyNav extends React.Component {
 
-        render() {
-            return (
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="//localhost:3000">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="//localhost:3000">
-                                        Home <span class="sr-only">(current)</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="//localhost:3000">
-                                        Link
-                                    </a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="//"
-                                       id="navbarDropdown" role="button"
-                                       data-toggle="dropdown" aria-haspopup="true"
-                                       aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="//localhost:3000">
-                                            Action
-                                        </a>
-                                        <a class="dropdown-item" href="//localhost:3000">
-                                            Another action
-                                        </a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="//localhost:3000">
-                                            Something else here
-                                        </a>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" href="//localhost:3000">Disabled</a>
-                                </li>
-                            </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                                    Search
-                                </button>
-                            </form>
-                        </div>
-                </nav>
-            );
-        }
+    render() {
+        const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+
+        return (
+            <Nav justify variant="tabs" activeKey="1" onSelect={handleSelect}>
+                <Nav.Item>
+                    <Nav.Link eventKey="1" href="#/home">
+                        NavLink 1 content
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="2" title="Item">
+                        NavLink 2 content
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="3" disabled>
+                        NavLink 3 content
+                    </Nav.Link>
+                </Nav.Item>
+                <NavDropdown title="Dropdown" id="nav-dropdown">
+                    <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+        );
     }
 
+}
 
-export default Nav;
+export default MyNav;
