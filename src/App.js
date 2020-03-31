02210ed790aa1505311fec14cd2302459e48af3e
Erik Dobeš <dobes@simplo.cz>
2020-03-31 17:16:08 +0100

    feat : diffrent nav bar for loged member and unloged visitor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-import Nav      from './components/nav/nav.component';
+import HomePage     from './pages/homepage/homepage.component';
+import NavUser      from './components/nav/nav_user.component';
+import NavVisitor   from './components/nav/nav_visitor.component';
 
 function App() {
+
+    const [user, setUser] = useState(false);
+
   return (
     <div>
-        <Nav></Nav>
+        {user ? <NavUser></NavUser> : <NavVisitor></NavVisitor>}
       <HomePage></HomePage>
     </div>
   );
