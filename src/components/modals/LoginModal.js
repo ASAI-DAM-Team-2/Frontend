b397d8385bb8b4dd4da3b2cc4fb31601eecdc8e2
Erik Dobeš <dobes@simplo.cz>
2020-05-02 20:34:34 +0100

    feat : better look

--- src/components/modals/LoginModal.js
+++ src/components/modals/LoginModal.js
@@ -4,10 +4,23 @@ import {
     Modal,
     ModalHeader,
     ModalBody,
-    ModalFooter
+    ModalFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText,
+    Row,
+    Col
 } from 'reactstrap';
 
 class LoginModal extends Component {
+
+    handleLoginClick = () => {
+        this.props.onLogin();
+        this.props.onLoginModalToggle();
+    };
+
     render () {
         return (
             <React.Fragment>
@@ -18,20 +31,36 @@ class LoginModal extends Component {
                     isOpen={this.props.show}
                     toggle={this.props.onLoginModalToggle}
                 >
-                <ModalHeader
-                    toggle={this.props.onLoginModalToggle}
-                >
-                    <span id="contained-modal-title-vcenter">
-                        Login
-                    </span>
+                <ModalHeader toggle={this.props.onLoginModalToggle}>
+                        <i className='nc-icon nc-book-bookmark' />&nbsp;Login
                 </ModalHeader>
                 <ModalBody>
-                    <h4>Centered Modal</h4>
-                    <p>
-                        Some lovely text
-                    </p>
+                    <Form>
+                        <Row form>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="userName">Your user name</Label>
+                                    <Input type="text" name="username" id="userName" placeholder="Enter your user name" required/>
+                                </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="password">Your password</Label>
+                                    <Input type="password" name="password" id="password" placeholder="Enter your password" required/>
+                                </FormGroup>
+                            </Col>
+                            <Col md={12} className="text-center">
+                                <Button color="info" onClick={this.handleLoginClick} className="w-100">
+                                    Submit
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
                 </ModalBody>
                 <ModalFooter>
+                    <div className="mr-auto ml-2">
+                        New to DISH DELIVERY? Create an account.
+                    </div>
                     <Button color="danger" onClick={this.props.onLoginModalToggle}>Close</Button>
                 </ModalFooter>
                 </Modal>
