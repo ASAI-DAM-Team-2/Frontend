ff65ea3875d3334d7c93e8a6bdf19aa3b7d1fd41
Dragino <dracossos@gmail.com>
2020-05-12 23:53:14 +0100

    Restaurants implementation

--- src/pages/restaurants/RestaurantsPage.js
+++ src/pages/restaurants/RestaurantsPage.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import IndexHeader from '../../components/Headers/IndexHeader';
 
 class RestaurantsPage extends Component {
   state = {};
   render() {
-    return <div className='restaurants-layout'>Hello World!</div>;
+    return (
+      <React.Fragment>
+        <IndexHeader />
+        <div className='restaurants-layout'>Hello World!</div>
+      </React.Fragment>
+    );
   }
 }
 
