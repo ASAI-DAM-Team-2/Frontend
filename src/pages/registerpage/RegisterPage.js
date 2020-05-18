451f9f6baedb2e7033362bbc747abf29a76db51c
Dragino <dracossos@gmail.com>
2020-05-18 19:44:16 +0100

    Fix registerpage layout

--- src/pages/registerpage/RegisterPage.js
+++ src/pages/registerpage/RegisterPage.js
@@ -12,87 +12,160 @@ import {
 } from 'reactstrap';
 import Alert from '../../components/alert/Alert';
 
-function RegisterPage() {
-  document.documentElement.classList.remove('nav-open');
-  React.useEffect(() => {
+class RegisterPage extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+    firstName: '',
+    lastName: '',
+  };
+
+  componentDidMount(prevProps) {
     document.body.classList.add('register-page');
     return function cleanup() {
       document.body.classList.remove('register-page');
     };
-  });
-  return (
-    <React.Fragment>
-      <div
-        className='page-header'
-        style={{
-          backgroundImage:
-            'url(' + require('../../assets/img/login-image.jpg') + ')',
-        }}
-      >
-        <div className='filter' />
-        <Container>
-          <Row>
-            <Col className='ml-auto mr-auto' lg='4'>
-              <Card className='card-register ml-auto mr-auto'>
-                <h3 className='title mx-auto'>Welcome</h3>
-                <div className='social-line text-center'>
-                  <Button
-                    className='btn-neutral btn-just-icon mr-1'
-                    color='facebook'
-                    href='#pablo'
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className='fa fa-facebook-square' />
-                  </Button>
-                  <Button
-                    className='btn-neutral btn-just-icon mr-1'
-                    color='google'
-                    href='#pablo'
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className='fa fa-google-plus' />
-                  </Button>
-                  <Button
-                    className='btn-neutral btn-just-icon'
-                    color='twitter'
-                    href='#pablo'
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className='fa fa-twitter' />
-                  </Button>
-                </div>
-                <Form className='register-form'>
-                  <label>Email</label>
-                  <Input placeholder='Email' type='text' />
-                  <label>Password</label>
-                  <Input placeholder='Password' type='password' />
-                  <Button block className='btn-round' color='danger'>
-                    Register
-                  </Button>
-                </Form>
-                <div className='forgot'>
-                  <Button
-                    className='btn-link'
-                    color='danger'
-                    href='#pablo'
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Forgot password?
-                  </Button>
-                </div>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-        <div className='footer register-footer text-center'>
-          <h6>
-            © {new Date().getFullYear()}, made with{' '}
-            <i className='fa fa-heart heart' /> by Creative Tim
-          </h6>
+  }
+
+  componentDidUpdate(prevProps) {
+    document.body.classList.add('register-page');
+    return function cleanup() {
+      document.body.classList.remove('register-page');
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div
+          className='page-header'
+          style={{
+            backgroundImage:
+              'url(' + require('../../assets/img/login-image.jpg') + ')',
+          }}
+        >
+          <div className='filter' />
+          <Container>
+            <Row>
+              <Col className='ml-auto mr-auto' lg='4'>
+                <Card className='card-register ml-auto mr-auto'>
+                  <h3 className='title mx-auto'>Welcome</h3>
+                  <div className='social-line text-center'>
+                    <Button
+                      className='btn-neutral btn-just-icon mr-1'
+                      color='facebook'
+                      href='#pablo'
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className='fa fa-facebook-square' />
+                    </Button>
+                    <Button
+                      className='btn-neutral btn-just-icon mr-1'
+                      color='google'
+                      href='#pablo'
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className='fa fa-google-plus' />
+                    </Button>
+                    <Button
+                      className='btn-neutral btn-just-icon'
+                      color='twitter'
+                      href='#pablo'
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className='fa fa-twitter' />
+                    </Button>
+                  </div>
+                  <Form onSubmit={this.handleSubmit} className='register-form'>
+                    <div className='input-field'>
+                      <label htmlFor='email'>Email</label>
+                      <Input
+                        placeholder='Email'
+                        type='email'
+                        id='email'
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className='input-field'>
+                      <label htmlFor='password'>Password</label>
+                      <Input
+                        placeholder='Password'
+                        type='password'
+                        id='password'
+                        onChange={this.handleChange}
+                      />
+                    </div>
+
+                    <div className='input-field'>
+                      <label htmlFor='confirmPassword'>Confirm password</label>
+                      <Input
+                        placeholder='Confirm password'
+                        type='password'
+                        id='confirmPassword'
+                        onChange={this.handleChange}
+                      />
+                    </div>
+
+                    <div className='input-field'>
+                      <label htmlFor='firstName'>First name</label>
+                      <Input
+                        placeholder='First name'
+                        type='text'
+                        id='firstName'
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className='input-field'>
+                      <label htmlFor='lastName'>Last name</label>
+                      <Input
+                        placeholder='Last name'
+                        type='text'
+                        id='lastName'
+                        onChange={this.handleChange}
+                      />
+                    </div>
+
+                    <div>
+                      <Button block className='btn-round' color='danger'>
+                        Register
+                      </Button>
+                    </div>
+                  </Form>
+                  <div className='forgot'>
+                    <Button
+                      className='btn-link'
+                      color='danger'
+                      href='#pablo'
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Forgot password?
+                    </Button>
+                  </div>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+          <div className='footer register-footer text-center'>
+            <h6>
+              © {new Date().getFullYear()}, made with{' '}
+              <i className='fa fa-heart heart' /> by Creative Tim
+            </h6>
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default RegisterPage;
